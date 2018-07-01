2a053d9f6fad7182e2e6c7535a1a7f9d92d9c831
Justin Joy <JJ@Dev-Team.Dlink>
2018-07-01 10:24:05 +0530

    expect statement added to second test case

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,7 +5,7 @@ const utils = require('./utils');
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
 
-    expect(res).toBe(44);
+    expect(res).toBe(44).toBeA('number');
     // if (res !== 44) {
     //     throw new Error(`Expected 44, but got ${res}`)
     // }
@@ -14,8 +14,9 @@ it('should add two numbers', () => {
 it('should square a number', () => {
     var res = utils.square(2);
 
-    if (res !== 4) {
-        throw new Error(`Expected 4, but got ${res}`)
-    }
+    expect(res).toBe(4).toBeA('number');
+    // if (res !== 4) {
+    //     throw new Error(`Expected 4, but got ${res}`)
+    // }
 
 });
