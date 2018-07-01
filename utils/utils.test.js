be340f1e84edccfd38cc2d6b675e06d26f56b425
Justin Joy <JJ@Dev-Team.Dlink>
2018-07-01 14:19:14 +0530

    Async Testing Code Added

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -45,3 +45,17 @@ it('should verify first and last names are set', () => {
         lastName: 'Joy'
     });
 });
+
+it('should async add', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    });
+});
+
+it('should async square a number', (done) => {
+    utils.asyncSquare(4, (square) => {
+        expect(square).toBe(16).toBeA('number');
+        done();
+    });
+});
